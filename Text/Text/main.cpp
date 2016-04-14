221526276412bd0d081460f8ea72d9a860b993bb
TheYoungSmile <1069230922@qq.com>
2016-04-14 22:32:11 +0800

    test
    test

---
+++ Text/Text/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Text
+//
+//  Created by caoyang on 16/4/14.
+//  Copyright © 2016年 neworigin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
